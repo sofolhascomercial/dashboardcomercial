146e255a50ba316d8dbc93a2c13ed169aa59f804
sofolhascomercial <sofolhascorporativo@gmail.com>
2026-04-15 09:54:58 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+window.__SOFOLHAS_FIREBASE__ = {
+  apiKey: "AIzaSyDA893dkJJVMc27Yx_Yb5uRcZulMaEsfP8",
+  authDomain: "dashboard-comercial-94395.firebaseapp.com",
+  databaseURL: "https://dashboard-comercial-94395-default-rtdb.firebaseio.com/",
+  projectId: "dashboard-comercial-94395",
+  storageBucket: "dashboard-comercial-94395.firebasestorage.app",
+  messagingSenderId: "972313202844",
+  appId: "1:972313202844:web:ed2fdd3dad3e5d47e0dd59",
+  measurementId: "G-36TW83D373"
+};
